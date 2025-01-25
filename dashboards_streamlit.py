9e1f2ca79888c291a3c81e41923635e90ddd5890
bmfranzini <bruno.m.franzini@gmail.com>
2025-01-24 21:34:37 -0300

    Including consolidated dashs

--- dashboards_streamlit.py
+++ dashboards_streamlit.py
@@ -15,7 +15,7 @@ def ultimo_dia_mes(mes, ano):
         return 29 if (ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)) else 28
 
 # Função para fazer requisição SOAP e obter dados XML
-def realizar_requisicao_soap(mes, ano):
+def realizar_requisicao_soap(mes, ano, filial):
     ultimo_dia = ultimo_dia_mes(mes, ano)
     url = "https://gaivota.dealernetworkflow.com.br/aws_dealernetgateway.aspx"
     headers = {"Content-Type": "text/xml; charset=utf-8"}
@@ -26,7 +26,7 @@ def realizar_requisicao_soap(mes, ano):
         <deal:WS_DealernetGateway.CONSULTASALDOCONTABIL>
             <deal:Usuario_identificador>portus</deal:Usuario_identificador>
             <deal:Usuariosenha_senha>Portus25@</deal:Usuariosenha_senha>
-            <deal:Empresa_codigo>1</deal:Empresa_codigo>
+            <deal:Empresa_codigo>{filial}</deal:Empresa_codigo>
             <deal:Dtini>{ano}-{mes:02d}-01</deal:Dtini>
             <deal:Dtfin>{ano}-{mes:02d}-{ultimo_dia}</deal:Dtfin>
         </deal:WS_DealernetGateway.CONSULTASALDOCONTABIL>
@@ -116,11 +116,8 @@ def obter_valor_conta(cdata_root, conta_id):
     return None
 
 # Função principal de análise
-def analise_margens(tipo, mes, ano):
-    cdata_root = realizar_requisicao_soap(mes, ano)
-    if cdata_root is None:
-        return
-
+def analise_margens(tipo, mes, ano, filial, num_filiais):
+    
     contas = [
         {"nome": "VN Passageiros", "receita": "3.1.1.001.000001", "custo": "3.3.1.001.000001"},
         {"nome": "VN Comerciais Leves", "receita": "3.1.1.001.000002", "custo": "3.3.1.001.000002"},
@@ -136,39 +133,53 @@ def analise_margens(tipo, mes, ano):
         {"nome": "Pneus e Câmaras", "receita": "3.1.1.003.000009", "custo": "3.3.1.003.000009"},
     ]
 
+    iterations = num_filiais if filial == 0 else 1
+
     if tipo == "Análise Subsetorial":
         titulo = f"Resultados e Margens Brutas por Subsetor - {mes:02d}/{ano}"
-        nomes, resultados, margens = calcular_resultados_margens(contas, cdata_root)
+        for i in range(iterations):
+            cdata_root = realizar_requisicao_soap(mes, ano, i+1)
+            if cdata_root is None:
+                return
+            if i == 0:
+                nomes, resultados, margens = calcular_resultados_margens(contas, cdata_root)
+            else:
+                _, res, mgs = calcular_resultados_margens(contas, cdata_root)
+                resultados = [a + b for a, b in zip(resultados, res)]
+                margens = [a + b for a, b in zip(margens, mgs)]
+        
     else:
-        # Consolidar os subsetores para os setores "Vendas" e "Pós-Vendas"
-        vendas_receita = sum(
-            obter_valor_conta(cdata_root, conta["receita"]) for conta in contas[:3]
-        )
-        vendas_custo = sum(
-            obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[:3]
-        )
-
-        pos_vendas_receita = sum(
-            obter_valor_conta(cdata_root, conta["receita"]) for conta in contas[3:]
-        )
-        pos_vendas_custo = sum(
-            obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[3:]
-        )
-
-        titulo = f"Resultados e Margens Brutas por Setor - {mes:02d}/{ano}"
+        for i in range(iterations):
+            cdata_root = realizar_requisicao_soap(mes, ano, i+1)
+            if cdata_root is None:
+                return
+            # Consolidar os subsetores para os setores "Vendas" e "Pós-Vendas"
+            vendas_receita = sum(obter_valor_conta(cdata_root, conta["receita"]) for conta in contas[:3])
+            vendas_custo = sum(obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[:3])
+            # gambiarra pois Ourinhos não tem "Peças Atacado"
+            if filial == 3 or iterations == 3:
+                pos_vendas_receita = sum(obter_valor_conta(cdata_root, conta["receita"]) for conta in contas[4:])
+                pos_vendas_custo = sum(obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[4:])
+            else:
+                pos_vendas_receita = sum(obter_valor_conta(cdata_root, conta["receita"]) for conta in contas[3:])
+                pos_vendas_custo = sum(obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[3:])
+            res = [(-vendas_receita - vendas_custo)/1000, (-pos_vendas_receita - pos_vendas_custo)/1000] # em milhares de reais
+            mgs = [(vendas_receita + vendas_custo)/vendas_receita * 100 if vendas_receita != 0 else 0,
+                    (pos_vendas_receita + pos_vendas_custo)/pos_vendas_receita * 100 if pos_vendas_receita != 0 else 0]
+            resultados = [a + b for a, b in zip(resultados, res)] if i > 0 else res
+            margens = [a + b for a, b in zip(margens, mgs)] if i > 0 else mgs
+        titulo = f"Resultados e Margens Brutas por Setor"
         nomes = ["Vendas", "Pós-Vendas"]
-        resultados = [(-vendas_receita - vendas_custo)/1000, (-pos_vendas_receita - pos_vendas_custo)/1000] # em milhares de reais
-        margens = [(vendas_receita + vendas_custo)/vendas_receita * 100 if vendas_receita != 0 else 0,
-                   (pos_vendas_receita + pos_vendas_custo)/pos_vendas_receita * 100 if pos_vendas_receita != 0 else 0]
+        
 
     grafico = criar_grafico(nomes, resultados, margens, titulo)
     return grafico
 
-def analise_margens_ano(ano):
+def analise_margens_ano(ano, filial):
     margens_ano = []
     resultados_ano = []
-    for i in range(1,13):
-        cdata_root = realizar_requisicao_soap(i, ano)
+    for i in range(7,13): #começando a partir de julho, troca de sistema
+        cdata_root = realizar_requisicao_soap(i, ano, filial)
         if cdata_root is None:
             return
 
@@ -201,9 +212,8 @@ def analise_margens_ano(ano):
         pos_vendas_custo = sum(
             obter_valor_conta(cdata_root, conta["custo"]) for conta in contas[3:]
         )
-
-        titulo = f"Resultados e Margens Brutas por Setor - {ano}"
-        meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        titulo = f"Resultados e Margens Brutas por Setor"
+        meses = ["Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
         resultados_ano.append([(-vendas_receita - vendas_custo)/1000, (-pos_vendas_receita - pos_vendas_custo)/1000]) # em milhares de reais
         margens_ano.append([(vendas_receita + vendas_custo)/vendas_receita * 100 if vendas_receita != 0 else 0,
                 (pos_vendas_receita + pos_vendas_custo)/pos_vendas_receita * 100 if pos_vendas_receita != 0 else 0])
@@ -234,11 +244,11 @@ def criar_grafico_anual(meses, resultados, margens, titulo):
             hovertemplate="<b>%{x}</b><br>Resultado: %{y:.2f} mil R$<extra></extra>"
         )
     )
-
+    
     # Ajustar layout
     fig.update_layout(
         barmode='group',  # Define agrupamento das barras
-        title=f"Resultados e Margens Mensais por Setor - {ano}",
+        title=titulo,
         xaxis_title="Meses",
         yaxis_title="Valores",
         legend_title="Indicadores",
@@ -252,24 +262,32 @@ def criar_grafico_anual(meses, resultados, margens, titulo):
 st.set_page_config(layout="wide")
  
 # Layout ajustado com espaçamento adicional
-col_param, col_space, col_graficos = st.columns([0.8, 0.2, 3])  # Aumente ou diminua os valores conforme necessário
-col_graficos.title("Análise de Margens Contábeis")
-col_graficos_esq, col_graficos_dir = col_graficos.columns([1,1.5])
+col_param, col_space, col_graficos = st.columns([0.6, 0.2, 3])  # Aumente ou diminua os valores conforme necessário
+
 # Coluna de parâmetros (lado esquerdo)
 with col_param:
     st.markdown("### Selecione os parâmetros de análise")
     ano = st.number_input("Ano", min_value=2000, max_value=2100, value=2024, step=1)
     mes = st.number_input("Mês", min_value=1, max_value=12, value=11, step=1)
-    col_graficos_esq.subheader("Análise Setorial")
+    filiais = ["Grupo Consolidado", "Araras", "Assis", "Ourinhos"]
+    # Caixa de seleção com várias possibilidades
+    filial = st.selectbox(
+        "Filial",
+        filiais
+    )
+    col_graficos.title(f"Análise de Margens Contábeis - {filial} - {mes:02d}/{ano}")
+    col_graficos_esq, col_graficos_dir = col_graficos.columns([1,2])
+    col_graficos_esq.subheader(f"Análise Setorial")
     col_graficos_dir.subheader("Análise Subsetorial")
 
     if st.button("Analisar"):
-        grafico_setorial = analise_margens(tipo="Análise Setorial", mes=mes, ano=ano)
-        grafico_subsetorial = analise_margens(tipo="Análise Subsetorial", mes=mes, ano=ano)
-        grafico_anual = analise_margens_ano(ano=ano)
+        indice_filial = filiais.index(filial)
+        grafico_setorial = analise_margens(tipo="Análise Setorial", mes=mes, ano=ano, filial=indice_filial, num_filiais=len(filiais)-1)
+        grafico_subsetorial = analise_margens(tipo="Análise Subsetorial", mes=mes, ano=ano, filial=indice_filial, num_filiais=len(filiais)-1)
+        #grafico_anual = analise_margens_ano(ano=ano, filial=indice_filial)
         col_graficos_esq.plotly_chart(grafico_setorial, use_container_width=True)
         col_graficos_dir.plotly_chart(grafico_subsetorial, use_container_width=True)
-        col_graficos.plotly_chart(grafico_anual, use_container_width=True)
+        #col_graficos.plotly_chart(grafico_anual, use_container_width=True)
 
 # Recuperar parâmetros selecionados
 #ano = st.session_state.get('ano', 2024)
